38800816ee6271e1ca781e5cf90cfcafb2bb1fa1
Harish krishnan <harishkrishnan1993@gmail.com>
2021-02-27 15:45:01 +0530

    Enable eureka server

---
+++ src/main/java/com/example/photoapp/discovery/photoapp/PhotoappApplication.java
@@ -0,0 +1,15 @@
+package com.example.photoapp.discovery.photoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class PhotoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappApplication.class, args);
+    }
+
+}
